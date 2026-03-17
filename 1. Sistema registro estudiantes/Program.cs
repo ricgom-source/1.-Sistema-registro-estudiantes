836c680680b4ab5bcc5763d144a4b73c9ef79792
ricogm-source <ricardoggmenchu@gmail.com>
2026-03-17 08:18:19 -0600

    Agregar archivos de proyecto.

---
+++ 1. Sistema registro estudiantes/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Sistema_registro_estudiantes
+{
+    internal class Program
+
+    {
+        class Estudiante
+
+        {
+            public string nombre;
+
+            public double nota1;
+
+            public double nota2;
+
+            public double nota3;
+
+
+            public double calcularProm() 
+            
+            {
+                
+                return (nota1+nota2+nota3)/3;
+            
+            
+            }
+            
+               
+
+
+            
+          
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+
+        {
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
